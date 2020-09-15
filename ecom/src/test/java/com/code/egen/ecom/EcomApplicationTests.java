88f5d9c00d09eeac85c03ac5aa9386f08fedbdbf
Srikumar Ramaswamy <srikumar4794@gmail.com>
2020-09-15 15:11:37 -0500

    Initial project setup

---
+++ ecom/src/test/java/com/code/egen/ecom/EcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.egen.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
