b127120bb6e60c733d3cb8cd1856d23dcab33e40
Srikumar Ramaswamy <srikumar4794@gmail.com>
2020-09-16 21:38:13 -0500

    Added swagger endpoints.

--- ecom/src/main/java/com/code/egen/ecom/EcomApplication.java
+++ ecom/src/main/java/com/code/egen/ecom/EcomApplication.java
@@ -3,12 +3,26 @@ package com.code.egen.ecom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class EcomApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcomApplication.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
 	}
 
 }
